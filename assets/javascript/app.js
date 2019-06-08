0e98d23d796e39636da61711c587ef41f43fd187
Brian Christopher <bcwood79@yahoo.com>
2019-06-07 23:45:30 -0400

    Most functions coded. Clicked still not working.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //SET GLOBAL VARIABLES
 
 var card = $("#build");
-var counterStartNumber = 3;
+var counterStartNumber = 30;
 var timer = null;
 
 
@@ -14,14 +14,14 @@ var questionsArray = [
         q: "What color is the sky?",
         a: ["Blue", "Green", "Purple", "Orange"],
         correct: "Blue",
-        image: "assets/images/CrystalCox.jpg"
+        image: "assets/images/sky.jpg"
     },
 
     question1 = {
         q: "What is the best beverage?",
         a: ["Water", "Ice water", "Warm water", "Beer"],
         correct: "Beer",
-        image: "assets/images/CrystalEastman.jpeg"
+        image: "assets/images/beer.jpg"
     },
 
     question2 = {
@@ -29,7 +29,22 @@ var questionsArray = [
         a: ["Minnesota", "Mississippi", "Confusion", "Florida"],
         correct: "Confusion",
         image: "assets/images/CrystalReed.jpg"
+    },
+
+    question3 = {
+        q: "What color is Big Bird?",
+        a: ["Red", "Orange", "Yellow", "Blue"],
+        correct: "Yellow",
+        image: "assets/images/bigBird.jpg"
+    },
+
+    question4 = {
+        q: "What color is Cookie Monster?",
+        a: ["Red", "Orange", "Yellow", "Blue"],
+        correct: "Blue",
+        image: "assets/images/cookieMonster.jpg"
     }
+
 ];
 
 
@@ -67,7 +82,7 @@ var game = {
         for (i = 0; i < this.questions[this.currentQuestion].a.length; i++) {
             console.log(this.questions[this.currentQuestion].a[i]);
 
-            var answerChoice = $("<button>").text(this.questions[this.currentQuestion].a[i]).attr({ type: "button", class: "answer-button btn btn-outline-secondary text-left w-100 mt-1" });
+            var answerChoice = $("<button>").text(this.questions[this.currentQuestion].a[i]).attr({ type: "button", class: "answer-button btn btn-outline-secondary text-left w-100 mt-1", value: this.questions[this.currentQuestion].a[i] });
             $("#build").append(answerChoice);
         }
     },//end of LOAD QUESTION
@@ -100,19 +115,15 @@ var game = {
         var question = $("<h3>").text(this.questions[this.currentQuestion].q);
         $("#build").append(question);
         //Time-up Notice
-        var answerChoice = $("<p>").text("TIME IS UP!").attr("class", "alert alert-danger");
-        $("#build").append(answerChoice);
+        var notice = $("<p>").text("TIME IS UP!").attr("class", "alert alert-danger");
+        $("#build").append(notice);
 
         //Show correct answer
         var answerChoice = $("<p>").text(this.questions[this.currentQuestion].correct).attr("class", "alert alert-info");
         $("#build").append(answerChoice);
         //Show image
-        var correctImage = $("<img>").attr({"src":this.questions[this.currentQuestion].image, "class":"img-thumbnail"})
+        var correctImage = $("<img>").attr({ "src": this.questions[this.currentQuestion].image, "class": "img-thumbnail" })
         $("#imageHere").append(correctImage);
-
-
-    
-
 
     },//end of TIMEUP
 
@@ -123,21 +134,87 @@ var game = {
 
     clicked: function () {
         console.log("The click function has been called.");
+        console.log($(this));
+        clearInterval(timer);
+        console.log("You clicked");
 
     },//end of CLICKED
 
     answerIncorrectly: function () {
         console.log("The answerInncorrectly function has been called.");
+        //add point to incorrect
+        this.incorrect++;
+        clearInterval(timer);
+        //Update scoreboard
+        $("#counterDisplay").text(counterStartNumber);
+        $("#correct").text("Correct: " + game.correct);
+        $("#incorrect").text("Wrong: " + game.incorrect);
+        //Clear question & answers
+        $("#build").text("");
+        //Reshow current question
+        var question = $("<h3>").text(this.questions[this.currentQuestion].q);
+        $("#build").append(question);
+        //Correct Notice
+        var notice = $("<p>").text("Whoops! That's wrong.").attr("class", "alert alert-danger");
+        $("#build").append(notice);
+
+        //Show correct answer
+        var answerChoice = $("<p>").text(this.questions[this.currentQuestion].correct).attr("class", "alert alert-info");
+        $("#build").append(answerChoice);
+        //Show image
+        var correctImage = $("<img>").attr({ "src": this.questions[this.currentQuestion].image, "class": "img-thumbnail" })
+        $("#imageHere").append(correctImage);
+
+        if (this.currentQuestion == this.questions.lenth) {
+            setTimeout(this.results(), 3000);
+        }
+
+        else  {
+            setTimeout(this.nextQuestion(), 3000)
+        }
 
     },//end of ANSWERINCORRECTLY
 
     answerCorrectly: function () {
         console.log("The answerCorrectly function has been called.");
+        //add point to correct
+        this.correct++;
+        clearInterval(timer);
+        //Update scoreboard
+        $("#counterDisplay").text(counterStartNumber);
+        $("#correct").text("Correct: " + game.correct);
+        $("#incorrect").text("Wrong: " + game.incorrect);
+        //Clear question & answers
+        $("#build").text("");
+        //Reshow current question
+        var question = $("<h3>").text(this.questions[this.currentQuestion].q);
+        $("#build").append(question);
+        //Correct Notice
+        var notice = $("<p>").text("Correct!").attr("class", "alert alert-success");
+        $("#build").append(notice);
+
+        //Show correct answer
+        var answerChoice = $("<p>").text(this.questions[this.currentQuestion].correct).attr("class", "alert alert-info");
+        $("#build").append(answerChoice);
+        //Show image
+        var correctImage = $("<img>").attr({ "src": this.questions[this.currentQuestion].image, "class": "img-thumbnail" })
+        $("#imageHere").append(correctImage);
+
+        if (this.currentQuestion == this.questions.lenth) {
+            setTimeout(this.results(), 3000);
+        }
+
+        else  {
+            setTimeout(this.nextQuestion(), 3000)
+        }
 
     },//end of ANSWERCORRECTLY
 
     reset: function () {
         console.log("The reset function has been called.");
+
+
+
 
     }//end of RESET
 
@@ -161,8 +238,10 @@ $(document).on("click", "#start", function () {
 });
 
 //Answer Button
-$(document).on("click", ".answer-button", function () {
-    console.log("You clicked an answer button.");
+$(document).on("click", ".answer-button", function (event) {
+
+
+       console.log(event.target.nodeName);
     game.clicked();
 });
 
